f9f2104bd7a83c95a671eb07b1a9e21a28618b64
wnel89 <wnel8977@gmail.com>
2022-07-04 18:48:12 +0900

    ddd

---
+++ hello-react/src/Counter.js
@@ -0,0 +1,57 @@
+import React, {Component} from "react";
+
+class Counter extends Component{
+    state = {
+        number: 0
+    }
+
+    constructor(props){
+        super(props);
+        console.log('constructor');
+    }
+    componentWillUnmount(){
+        console.log('componentWillMount (deprecated)');
+    }
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        // 5의 배수라면 리렌더링 하지 않음
+        console.log('shouldComponentUpdate');
+        if(nextState.number % 5 === 0)
+        return false;
+        return true;
+    }
+    componentDidUpdate(prevProps, prevState){
+        console.log('componentDidUpdate');
+    }
+
+    handleIncrease = () => {
+        const {number} = this.state;
+        this.setState({
+            number: number + 1
+        })
+    }
+    
+    handleDecrease = () => {
+        this.setState(
+            ({number}) => ({
+                number: number - 1
+            })
+        )
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>카운터</h1>
+                <div>값: {this.state.number}</div>
+                <button onClick={this.handleIncrease}>+</button>
+                <button onClick={this.handleDecrease}>-</button>
+            </div>
+        )
+    }
+}
+
+export default Counter;
